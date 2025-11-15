0be399ea04e6e9bd1b2cc1ab4929cf7fd525afe5
Arsenymlwww <sentyaabrr@gmail.com>
2025-11-15 19:54:32 +0300

    complete task 02-01

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,46 @@
 */
 
 // тестовые данные (значения можно менять)
-const isAdmin = false
-const isVerifiedUser = true
-const hasSpecialPermission = true
-const hasTemporaryPass = false
+// const isAdmin = false
+// const isVerifiedUser = true
+// const hasSpecialPermission = true
+// const hasTemporaryPass = false
 
-let isAccess
+// let isAccess
 
 // your code
+
+const isAdmin = "админ"
+const isVerifiedUser = "сотрудник"
+const hasSpecialPermission = "разрешение"
+const hasTemporaryPass = "зеленая карточка"
+
+let isAccess = false;
+
+
+while (!isAccess) {
+        const userStatus = prompt ("Введите свой статус!").toLowerCase();
+        
+    if (userStatus === isAdmin) { 
+    alert ("Вы администратор! Проверим ваши разрешения") 
+    } else if (userStatus === isVerifiedUser) {
+    alert ("вы являетесь сотрудником ! Проверим Ваши разрешения !")
+    } else {
+    alert("введите корректные данные")
+    }
+
+    const userPerm = prompt ("У вас есть разрешение или пропуск?").toLowerCase();
+
+    if (userPerm === hasSpecialPermission || userPerm === hasTemporaryPass){
+    alert ("вы подтвердили наличие разрешения/пропуска !")
+}else {
+    alert ("у вас нет пропуска-разрешения, доступ запрещен!")
+}
+
+    if (( userStatus === isAdmin || userStatus === isVerifiedUser )&&( userPerm === hasSpecialPermission || userPerm === hasTemporaryPass)){
+    alert (" вы получили доступ! ")
+    isAccess = true
+}   else { 
+    alert ("доступ запрещен!")  }
+}
+
